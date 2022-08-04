1d2995f1bacb0a576b7f2f42fd0b113abfb69c73
Gedeon Nzemba <40776419+GedeonNzemba@users.noreply.github.com>
2022-08-05 00:28:50 +0200

    react serverless app init setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { Header } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
@@ -6,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header as='h1'>Hello World!</Header>
         <a
           className="App-link"
           href="https://reactjs.org"
